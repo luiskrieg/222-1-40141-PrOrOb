49a04259c778984d8d75ed4a04162f29ace34c12
luiskrieg <luiskrieg@gmail.com>
2021-10-26 20:28:22 -0500

    Python and Java constructor

---
+++ Java/Classes/User.java
@@ -0,0 +1,14 @@
+public class User{
+
+    int id;
+    String full_name;
+    String personal_email;
+
+    //This is Constructor
+    public User(int id, String full_name, String personal_email){
+        this.id = id;
+        this.full_name = full_name;
+        this.personal_email = personal_email;
+    }
+}
+
--- Java/Main.java
+++ Java/Main.java
@@ -1,14 +1,13 @@
-
-/**
- * Main class
- */
+//import User;
 
 public class Main {
 
     public static void main(String[] args) {
     
-        System.out.println("Learning Object Oriented Programming with Java");
-        
+        //System.out.println("Learning Object Oriented Programming with Java");
+        User object_user = new User(123, "Luis Guerra", "luis.guerra@gmail.com");
+        System.out.println(object_user.id);
+
     }
 
     
